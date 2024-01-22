7c9743a7b66baf615d40119ebe6bd88f3e9cd20d
Karl <karl.logon@gmail.com>
2024-01-22 18:09:05 +0800

    update

--- server/src/main/java/org/example/ReadDemo.java
+++ server/src/main/java/org/example/ReadDemo.java
@@ -1,2 +1,37 @@
-package org.example;public class ReadDemo {
+package org.example;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.util.List;
+
+public class ReadDemo {
+    public static void main(String[] args) {
+        // Create an instance of ObjectMapper
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            // Read system list from JSON content
+            SystemList systemList = objectMapper.readValue(
+                    "{\"systems\":[" +
+                            "{\"name\":\"AHU-2-01\"}," +
+                            "{\"name\":\"AHU-2M-01\"}," +
+                            "{\"name\":\"AHU-2M-02\"}," +
+                            "{\"name\":\"AHU-2M-03\"}," +
+                            "{\"name\":\"AHU-2M-04\"}" +
+                            "]}",
+                    SystemList.class);
+
+            // Access the system list
+            List<SystemList.SystemInfo> systems = systemList.getSystems();
+
+            // Display system names
+            for (SystemList.SystemInfo system : systems) {
+                System.out.println("System Name from ReadDemo: " + system.getName());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- server/src/main/java/org/example/SystemList.java
+++ server/src/main/java/org/example/SystemList.java
@@ -1,2 +1,53 @@
-package org.example;public class SystemList {
+package org.example;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.util.List;
+
+class SystemList {
+    @JsonProperty("systems")
+    private List<SystemInfo> systems;
+
+    public List<SystemInfo> getSystems() {
+        return systems;
+    }
+
+    public static class SystemInfo {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    public static void main(String[] args) {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            // Read system list from JSON content
+            SystemList systemList = objectMapper.readValue(
+                    "{\"systems\":[" +
+                            "{\"name\":\"AHU-2-01\"}," +
+                            "{\"name\":\"AHU-2M-01\"}," +
+                            "{\"name\":\"AHU-2M-02\"}," +
+                            "{\"name\":\"AHU-2M-03\"}," +
+                            "{\"name\":\"AHU-2M-04\"}" +
+                            "]}",
+                    SystemList.class);
+
+            // Access the system list
+            List<SystemInfo> systems = systemList.getSystems();
+
+            // Display system names
+            for (SystemInfo system : systems) {
+                System.out.println("System Name: " + system.getName());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
